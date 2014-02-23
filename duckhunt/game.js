286e08d512523b1941c1ca97ada37895dbdf7994
Siddhartha Prasad <Siddhartha.Prasad@tufts.edu>
2014-02-23 13:55:23 -0500

    Tree from spritesheet drawn

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -15,8 +15,12 @@ function draw() {
 	var img = new Image();
 	//Once the image is loaded, do the following
 	img.onload = function () {
+		
+		//Drawing the tree
+		ctx.drawImage(img, 0, 272, 78, 122, 10, 100, 200, 400);
 		//Drawing the dirt and grass
 		ctx.drawImage(img, 0, 706, 900, 194, 0, 406, 800, 194);
+		
 	}
 	img.src = "assets/duckhunt.png";
 	
